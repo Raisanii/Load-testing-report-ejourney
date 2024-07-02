47aa41a79272cd985e63d9da562c7628105244c4
Raisanii <raisani254aprilia@gmail.com>
2024-07-03 05:17:05 +0700

    register dan login - cekout data

--- login-cekout/content/js/dashboard.js
+++ login-cekout/content/js/dashboard.js
@@ -133,7 +133,7 @@ $(document).ready(function() {
         widgets: ['zebra']
     });
 
-    var data = {"OkPercent": 100.0, "KoPercent": 0.0};
+    var data = {"OkPercent": 88.23809523809524, "KoPercent": 11.761904761904763};
     var dataset = [
         {
             "label" : "FAIL",
@@ -173,7 +173,7 @@ $(document).ready(function() {
     });
 
     // Creates APDEX table
-    createTable($("#apdexTable"), {"supportsControllersDiscrimination": true, "overall": {"data": [0.9642857142857143, 500, 1500, "Total"], "isController": false}, "titles": ["Apdex", "T (Toleration threshold)", "F (Frustration threshold)", "Label"], "items": [{"data": [1.0, 500, 1500, "view data in market place"], "isController": false}, {"data": [1.0, 500, 1500, "add to cart"], "isController": false}, {"data": [0.85, 500, 1500, "payment success"], "isController": false}, {"data": [1.0, 500, 1500, "get cart"], "isController": false}, {"data": [1.0, 500, 1500, "login"], "isController": false}, {"data": [1.0, 500, 1500, "market place - searching course"], "isController": false}, {"data": [0.9, 500, 1500, "cekout cart"], "isController": false}]}, function(index, item){
+    createTable($("#apdexTable"), {"supportsControllersDiscrimination": true, "overall": {"data": [0.23761904761904762, 500, 1500, "Total"], "isController": false}, "titles": ["Apdex", "T (Toleration threshold)", "F (Frustration threshold)", "Label"], "items": [{"data": [0.165, 500, 1500, "view data in market place"], "isController": false}, {"data": [0.325, 500, 1500, "add to cart"], "isController": false}, {"data": [0.005, 500, 1500, "payment success"], "isController": false}, {"data": [0.7316666666666667, 500, 1500, "get cart"], "isController": false}, {"data": [0.013333333333333334, 500, 1500, "login"], "isController": false}, {"data": [0.42333333333333334, 500, 1500, "market place - searching course"], "isController": false}, {"data": [0.0, 500, 1500, "cekout cart"], "isController": false}]}, function(index, item){
         switch(index){
             case 0:
                 item = item.toFixed(3);
@@ -187,7 +187,7 @@ $(document).ready(function() {
     }, [[0, 0]], 3);
 
     // Create statistics table
-    createTable($("#statisticsTable"), {"supportsControllersDiscrimination": true, "overall": {"data": ["Total", 70, 0, 0.0, 206.82857142857148, 75, 1017, 99.5, 401.3999999999999, 658.6, 1017.0, 6.972806056380118, 18.28493873891822, 2.464898614154796], "isController": false}, "titles": ["Label", "#Samples", "FAIL", "Error %", "Average", "Min", "Max", "Median", "90th pct", "95th pct", "99th pct", "Transactions/s", "Received", "Sent"], "items": [{"data": ["view data in market place", 10, 0, 0.0, 180.1, 78, 460, 156.0, 431.1000000000001, 460.0, 460.0, 1.1075423634954036, 14.987946824122274, 0.22172478956695094], "isController": false}, {"data": ["add to cart", 10, 0, 0.0, 87.4, 80, 107, 83.5, 105.9, 107.0, 107.0, 1.1556685542586387, 0.4074183086790708, 0.274245565121923], "isController": false}, {"data": ["payment success", 10, 0, 0.0, 257.3, 84, 663, 93.0, 662.2, 663.0, 663.0, 1.4044943820224718, 0.22219540028089887, 1.3592323560393258], "isController": false}, {"data": ["get cart", 10, 0, 0.0, 107.0, 75, 360, 79.5, 332.7000000000001, 360.0, 360.0, 1.1595547309833025, 0.7983262552179963, 0.21741651205936918], "isController": false}, {"data": ["login", 10, 0, 0.0, 327.90000000000003, 310, 406, 319.5, 399.5, 406.0, 406.0, 1.079097874177188, 1.2270523092694507, 0.43722432421495633], "isController": false}, {"data": ["market place - searching course", 10, 0, 0.0, 81.5, 77, 90, 80.5, 89.3, 90.0, 90.0, 1.1559357299734134, 2.2746196248988557, 0.2743089671714253], "isController": false}, {"data": ["cekout cart", 10, 0, 0.0, 406.59999999999997, 237, 1017, 260.5, 1017.0, 1017.0, 1017.0, 1.2399256044637321, 0.6441800991940484, 0.2966618877867328], "isController": false}]}, function(index, item){
+    createTable($("#statisticsTable"), {"supportsControllersDiscrimination": true, "overall": {"data": ["Total", 2100, 247, 11.761904761904763, 15949.023809523802, 76, 136684, 2364.5, 47969.200000000084, 69572.54999999999, 135747.79, 10.436545801526716, 27.071387409860545, 3.9666736398944415], "isController": false}, "titles": ["Label", "#Samples", "FAIL", "Error %", "Average", "Min", "Max", "Median", "90th pct", "95th pct", "99th pct", "Transactions/s", "Received", "Sent"], "items": [{"data": ["view data in market place", 300, 0, 0.0, 35679.67333333333, 164, 135929, 19704.0, 132930.5, 134668.95, 135856.99, 1.8381451889613254, 23.39229431346809, 0.42415559248014806], "isController": false}, {"data": ["add to cart", 300, 0, 0.0, 3222.180000000001, 707, 136062, 1371.0, 2483.400000000001, 4404.349999999993, 69899.28, 1.8473361412842682, 0.6638864257740339, 0.4948299122977167], "isController": false}, {"data": ["payment success", 300, 31, 10.333333333333334, 18753.28, 214, 80518, 11127.5, 48875.20000000003, 60167.99999999999, 75920.53, 1.8116598427479256, 0.3175358482191384, 1.7548272714440136], "isController": false}, {"data": ["get cart", 300, 0, 0.0, 1428.123333333333, 77, 134863, 418.0, 821.2000000000013, 1763.3999999999999, 35692.140000000145, 1.8477343697070108, 1.2168822485079547, 0.4029107494256626], "isController": false}, {"data": ["login", 300, 185, 61.666666666666664, 40461.06, 826, 136684, 21831.5, 134106.1, 135575.05, 136511.31, 2.086651689144542, 1.4305382996223162, 0.9118436937038764], "isController": false}, {"data": ["market place - searching course", 300, 0, 0.0, 8377.696666666667, 145, 136091, 1021.0, 12557.00000000001, 68310.7, 135198.2, 1.8348399407958311, 5.58106705116757, 0.4914826634383677], "isController": false}, {"data": ["cekout cart", 300, 31, 10.333333333333334, 3721.1533333333336, 76, 41331, 2572.0, 4860.900000000007, 8462.499999999996, 32241.330000000016, 1.8381339264378802, 0.9388723775955983, 0.496990247167742], "isController": false}]}, function(index, item){
         switch(index){
             // Errors pct
             case 3:
@@ -217,7 +217,7 @@ $(document).ready(function() {
     }, [[0, 0]], 0, summaryTableHeader);
 
     // Create error table
-    createTable($("#errorsTable"), {"supportsControllersDiscrimination": false, "titles": ["Type of error", "Number of errors", "% in errors", "% in all samples"], "items": []}, function(index, item){
+    createTable($("#errorsTable"), {"supportsControllersDiscrimination": false, "titles": ["Type of error", "Number of errors", "% in errors", "% in all samples"], "items": [{"data": ["400/Bad Request", 31, 12.550607287449393, 1.4761904761904763], "isController": false}, {"data": ["500/Internal Server Error", 163, 65.9919028340081, 7.761904761904762], "isController": false}, {"data": ["401/Unauthorized", 53, 21.45748987854251, 2.5238095238095237], "isController": false}]}, function(index, item){
         switch(index){
             case 2:
             case 3:
@@ -228,7 +228,7 @@ $(document).ready(function() {
     }, [[1, 1]]);
 
         // Create top5 errors by sampler
-    createTable($("#top5ErrorsBySamplerTable"), {"supportsControllersDiscrimination": false, "overall": {"data": ["Total", 70, 0, "", "", "", "", "", "", "", "", "", ""], "isController": false}, "titles": ["Sample", "#Samples", "#Errors", "Error", "#Errors", "Error", "#Errors", "Error", "#Errors", "Error", "#Errors", "Error", "#Errors"], "items": [{"data": [], "isController": false}, {"data": [], "isController": false}, {"data": [], "isController": false}, {"data": [], "isController": false}, {"data": [], "isController": false}, {"data": [], "isController": false}, {"data": [], "isController": false}]}, function(index, item){
+    createTable($("#top5ErrorsBySamplerTable"), {"supportsControllersDiscrimination": false, "overall": {"data": ["Total", 2100, 247, "500/Internal Server Error", 163, "401/Unauthorized", 53, "400/Bad Request", 31, "", "", "", ""], "isController": false}, "titles": ["Sample", "#Samples", "#Errors", "Error", "#Errors", "Error", "#Errors", "Error", "#Errors", "Error", "#Errors", "Error", "#Errors"], "items": [{"data": [], "isController": false}, {"data": [], "isController": false}, {"data": ["payment success", 300, 31, "400/Bad Request", 31, "", "", "", "", "", "", "", ""], "isController": false}, {"data": [], "isController": false}, {"data": ["login", 300, 185, "500/Internal Server Error", 132, "401/Unauthorized", 53, "", "", "", "", "", ""], "isController": false}, {"data": [], "isController": false}, {"data": ["cekout cart", 300, 31, "500/Internal Server Error", 31, "", "", "", "", "", "", "", ""], "isController": false}]}, function(index, item){
         return item;
     }, [[0, 0]], 0);
 
